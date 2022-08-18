1204ea7af9aa722b6e8cf0c04d25bad957512c58
Brunomgro <brunomgro@gmail.com>
2022-08-17 22:40:58 -0300

    [0.1.0] Adding first structure and dependencies to run augmented some augmented reality in the app

--- app/src/main/java/com/example/supervisorar/presentation/fragment/SupervisingScreenFragment.kt
+++ app/src/main/java/com/example/supervisorar/presentation/fragment/SupervisingScreenFragment.kt
@@ -5,24 +5,59 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.supervisorar.R
+import com.example.supervisorar.databinding.FragmentSupervisingScreenBinding
 import com.example.supervisorar.presentation.viewmodel.SupervisingScreenViewModel
+import io.github.sceneview.SceneView
+import io.github.sceneview.math.Position
+import io.github.sceneview.math.Rotation
+import io.github.sceneview.node.ModelNode
+import kotlinx.coroutines.delay
 import org.koin.android.ext.android.inject
 
 class SupervisingScreenFragment : Fragment() {
 
     private val viewModel: SupervisingScreenViewModel by inject()
+    private var _binding: FragmentSupervisingScreenBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_supervising_screen, container, false)
+    ): View {
+        _binding = FragmentSupervisingScreenBinding.inflate(inflater,container, false)
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel.info
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textView2.text = viewModel.info.value
 
+        val sceneView = binding.supervisorSceneView
+
+        sceneView.cameraNode.position = Position(x = 4.0f, y = -1.0f)
+        sceneView.cameraNode.rotation = Rotation(x = 0.0f, y = 80.0f)
+
+        val modelNode = ModelNode()
+
+        sceneView.addChild(modelNode)
+
+        lifecycleScope.launchWhenCreated {
+            modelNode.loadModel(
+                context = requireContext(),
+                lifecycle = lifecycle,
+                glbFileLocation = "https://sceneview.github.io/assets/models/MaterialSuite.glb",
+                autoAnimate = true,
+                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
+            )
+
+            sceneView.cameraNode.smooth(
+                position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
+                rotation = Rotation(x = -60.0f, y = -50.0f),
+                speed = 0.5f
+            )
+        }
+    }
 }

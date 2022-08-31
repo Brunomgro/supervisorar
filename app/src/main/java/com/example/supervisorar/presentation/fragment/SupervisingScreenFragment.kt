e2dbaba8a63d1b253e41867b0e3b2f7b868ed763
Brunomgro <brunomgro@gmail.com>
2022-08-31 03:20:21 -0300

    [0.3.0][AR] Creating structure to place a node in a specific place and modify its location when wanted

--- app/src/main/java/com/example/supervisorar/presentation/fragment/SupervisingScreenFragment.kt
+++ app/src/main/java/com/example/supervisorar/presentation/fragment/SupervisingScreenFragment.kt
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.example.supervisorar.databinding.FragmentSupervisingScreenBinding
 import com.example.supervisorar.presentation.viewmodel.SupervisingScreenViewModel
+import com.google.ar.core.Anchor
+import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.node.CursorNode
 import io.github.sceneview.math.Position
 import io.github.sceneview.math.Rotation
 import io.github.sceneview.node.ModelNode
@@ -18,6 +21,9 @@ class SupervisingScreenFragment : Fragment() {
     private val viewModel: SupervisingScreenViewModel by inject()
     private var _binding: FragmentSupervisingScreenBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var sceneView: ArSceneView
+    private lateinit var cursorNode: CursorNode
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,30 +37,57 @@ class SupervisingScreenFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val sceneView = binding.supervisorSceneView
-
-        sceneView.cameraNode.position = Position(x = 4.0f, y = -1.0f)
-        sceneView.cameraNode.rotation = Rotation(x = 0.0f, y = 80.0f)
-
-        val modelNode = ModelNode()
-
-        sceneView.addChild(modelNode)
+        setupArSceneView()
+        setupActionButton()
 
         lifecycleScope.launchWhenCreated {
-            modelNode.loadModel(
-                context = requireContext(),
-                lifecycle = lifecycle,
-                glbFileLocation = "https://sceneview.github.io/assets/models/MaterialSuite.glb",
-                autoAnimate = true,
-                centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f)
-            )
-
-            sceneView.cameraNode.smooth(
-                position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
-                rotation = Rotation(x = -60.0f, y = -50.0f),
-                speed = 0.5f
-            )
+            load3dImages(viewModel.modelNode.value!!, sceneView)
         }
+    }
+
+    private fun setupActionButton() {
+        binding.actionButton.setOnClickListener {
+            cursorNode.createAnchor()?.let { anchorOrMove(it) }
+        }
+    }
+
+    private fun setupArSceneView() {
+        sceneView = binding.supervisorSceneView
+        sceneView.isDepthOcclusionEnabled = true
+        sceneView.cameraNode.position = Position(x = 0.0f, y = 0.0f)
+        sceneView.cameraNode.rotation = Rotation(x = 0.0f, y = 80.0f)
+
+        sceneView.onTapAr = { hitResult, _ ->
+            anchorOrMove(hitResult.createAnchor())
+        }
+
+        cursorNode = CursorNode(context = requireContext(), lifecycle = lifecycle)
+
+        sceneView.addChild(cursorNode)
+    }
+
+    private suspend fun load3dImages(modelNode: ModelNode, sceneView: ArSceneView) {
+        modelNode.loadModel(
+            context = requireContext(),
+            lifecycle = lifecycle,
+            glbFileLocation = "https://sceneview.github.io/assets/models/Halloween.glb",
+            autoAnimate = true,
+            centerOrigin = Position(x = 0.0f, y = 0.0f, z = 0.0f),
+        )
+
+        sceneView.cameraNode.smooth(
+            position = Position(x = -1.0f, y = 1.5f, z = -3.5f),
+            rotation = Rotation(x = -60.0f, y = -50.0f),
+            speed = 0.5f
+        )
+    }
+
+    private fun anchorOrMove(anchor: Anchor) {
+        val modelNode = viewModel.modelNode.value!!
+
+        if (!sceneView.children.contains(modelNode)) {
+            sceneView.addChild(modelNode)
+        }
+        modelNode.anchor = anchor
     }
 }
--- app/src/main/java/com/example/supervisorar/presentation/viewmodel/SupervisingScreenViewModel.kt
+++ app/src/main/java/com/example/supervisorar/presentation/viewmodel/SupervisingScreenViewModel.kt
@@ -3,7 +3,18 @@ package com.example.supervisorar.presentation.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.supervisorar.domain.SupervisorarUseCase
+import io.github.sceneview.ar.node.ArModelNode
+import io.github.sceneview.ar.node.PlacementMode
+import io.github.sceneview.math.Position
 
 class SupervisingScreenViewModel(private val useCase: SupervisorarUseCase) : ViewModel() {
-    val info = MutableLiveData(useCase.getInfo())
+
+    val modelNode = MutableLiveData(
+        ArModelNode(
+            placementMode = PlacementMode.PLANE_VERTICAL,
+            hitPosition = Position(0.0f, 0.0f, 0.0f),
+            followHitPosition = true,
+            instantAnchor = false,
+        )
+    )
 }

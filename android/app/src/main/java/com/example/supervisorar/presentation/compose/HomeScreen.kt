3149185594945f4e18c272d707c7690a01304411
Brunomgro <brunomgro@gmail.com>
2025-02-04 17:16:48 -0300

    nova versao 2

--- android/app/src/main/java/com/example/supervisorar/presentation/compose/HomeScreen.kt
+++ android/app/src/main/java/com/example/supervisorar/presentation/compose/HomeScreen.kt
@@ -5,7 +5,11 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.supervisorar.models.Models3d
@@ -43,6 +47,7 @@ fun HomeScreen(
     val nodes = rememberNodes {
         add(CylinderNode(engine))
     }
+    var foundQrCode by remember { mutableStateOf(false) }
 
     ARScene(
         modifier = Modifier.fillMaxSize(),
@@ -70,6 +75,7 @@ fun HomeScreen(
         },
         onSessionUpdated = { session, updatedFrame ->
             runCatching {
+                if (foundQrCode) return@runCatching
                 viewModel.findQrCode(
                     image = { updatedFrame.acquireCameraImage() },
                     onFinish =  { info ->
@@ -80,11 +86,11 @@ fun HomeScreen(
                             bounds.centerY().toFloat()
                         ).firstOrNull() ?: return@findQrCode
 
-                        if (hitResult.trackable.isTracking) return@findQrCode
+                        //if (hitResult.trackable.isTracking) return@findQrCode
 
                         val anchor = hitResult.createAnchor() ?: return@findQrCode
                         val anchorNode = AnchorNode(engine, anchor)
-
+                        foundQrCode = true
                         coroutine.launch {
                             val model = modelLoader.loadModel(Models3d.Energymeter.path) ?: return@launch
                             nodes.add(

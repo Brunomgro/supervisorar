23f8ed550d0737fbe5ed4cb3ddb6788058019641
Brunomgro <brunomgro@gmail.com>
2025-02-04 15:22:42 -0300

    monorepo

--- ServidorPython/servidor.py
+++ ServidorPython/servidor.py
@@ -1,18 +1,29 @@
 import snap7
 from snap7.util import *
-
 import firebase_admin
 from firebase_admin import credentials, db
 
 # Inicializar o Firebase
-cred = credentials.Certificate("caminho/para/seu-arquivo.json")
+cred = credentials.Certificate("../../Firebase/supervisor-realidade-aumentada-firebase-adminsdk-fbsvc-e566db9f3a.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://seu-projeto.firebaseio.com'
+    'databaseURL': 'https://supervisor-realidade-aumentada-default-rtdb.firebaseio.com/'
 })
 
 # Enviar dados para o Firebase
-ref = db.reference('/dados')
-ref.set({'temperatura': 25, 'umidade': 60})
+db.reference('/Tanque/temperatura').set(80)
+db.reference('/Tanque/nivel').set(80)
+
+db.reference('/Tanque2/temperatura').set(50)
+db.reference('/Tanque2/nivel').set(50)
+
+db.reference('/motor123/temperatura').set(30)
+db.reference('/motor123/velocidadeTerminal').set(30)
+db.reference('/motor123/ligaDesligaTerminal').set(True)
+db.reference('/motor123/setVelocidade').set(6)
+db.reference('/motor123/setLigaDesliga').set(True)
+
+db.reference('/ParadaDeEmergencia').set(False)
+
 
 print("Dados enviados com sucesso!")
 
